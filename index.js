4d932498bac3e6702cbd78c7a64ec3c8a8d6cdf3
pkharade-equinix <pkharade@equinix.com>
2021-08-23 17:54:03 +0530

    create post schema and mongodb connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
 const resolvers = require("./src/resolvers/resolvers");
 const typeDefs = require("./src/typeDefs/typeDefs");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 4000;
 
 async function startServer() {
@@ -16,6 +17,11 @@ async function startServer() {
   app.use((req, res) => {
     res.json({ msg: "Hello world" });
   });
+  await mongoose.connect("mongodb://localhost:27017/postDBGPL", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  console.log("Mongoose connected");
   app.listen(port, () => {
     console.log(`Application is running on ${port}`);
   });
