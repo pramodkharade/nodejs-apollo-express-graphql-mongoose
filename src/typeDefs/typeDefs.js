539714d9c5195407ff9e56b81f6a87d6271e6906
pkharade-equinix <pkharade@equinix.com>
2021-08-23 17:57:35 +0530

    added typeDefs for list post

--- src/typeDefs/typeDefs.js
+++ src/typeDefs/typeDefs.js
@@ -1,7 +1,14 @@
 const { gql } = require("apollo-server-express");
 const typeDefs = gql`
+  type Post {
+    id: ID
+    title: String
+    description: String
+  }
   type Query {
     hello: String
+
+    getAllPosts: [Post]
   }
 `;
 module.exports = typeDefs;
